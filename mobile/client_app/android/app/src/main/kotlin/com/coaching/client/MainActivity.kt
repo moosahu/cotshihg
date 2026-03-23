a94b33fb2e040202e0f38b71565cab6c685ce17a
hussain almoussa <moosahu@hotmail.com>
2026-03-23 09:54:03 +0300

    fix: extend FlutterFragmentActivity for flutter_stripe Android support

--- mobile/client_app/android/app/src/main/kotlin/com/coaching/client/MainActivity.kt
+++ mobile/client_app/android/app/src/main/kotlin/com/coaching/client/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.coaching.client
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
